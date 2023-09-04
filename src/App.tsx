914870d4a45137a0d4dff2a48690ee17bf884636
igormarims <igormarquespe@gmail.com>
2023-09-04 09:15:07 -0300

    confirming the routes and structure of the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Search from './pages/Search'
+import Watch from './pages/Watch'
 
-
-function App() {
- 
-
+export default function App() {
   return (
-    <div >
-        <h1 className="text-3xl font-bold underline">Inicio do projeto</h1>
-    </div>
+     <BrowserRouter>
+        <Routes>
+           <Route path='/' element={<Home/>} ></Route>
+           <Route path='/search' element={<Search/>} ></Route>
+           <Route path='/watch' element={<Watch/>} ></Route>
+        </Routes>
+     </BrowserRouter>
   )
 }
 
-export default App
