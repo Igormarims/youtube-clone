a8a0b620b6c0ed6b8bf35587c89c4b7c8c263d7e
igormarims <igormarquespe@gmail.com>
2023-09-04 15:19:50 -0300

    NavBar Component II

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -20,27 +20,44 @@ export default function NavBar() {
                     </div>
                 </Link>
             </div>
-            <div className="flex items-center justify-center gap-5">
+            <div className="flex items-center justify-center gap-5 ">
                 <form action="">
                     <div className="flex bg-zinc-900 items-center h-10 px-4 pr-0">
                         <div className="flex gap-4 items-center pr-5">
-                            <div>
+                            
                                 <div>
-                                    <AiOutlineSearch className="text-xl " />
+                                    <AiOutlineSearch className="text-xl" />
                                 </div>
                                 <input type="text"
                                     className="w-96 bg-zinc-900 focus:outline-none border-none"
                                 />
 
-                                <AiOutlineClose className="text-xl cursor-pointer " />
-                            </div>
-                            <button className="h-10 w-16 items-center justify-center bg-zinc-800">
-                                 <AiOutlineSearch className="text-xl " />
-                            </button>
-                        
+                                <AiOutlineClose className="text-xl cursor-pointer" />
+                       
                         </div>
+                           <button className="h-10 w-16 items-center flex justify-center bg-zinc-800">
+                                 <AiOutlineSearch className="text-xl" />
+                            </button>
                     </div>
                 </form>
+                <div className="text-xl p-3 bg-zinc-900 rounded-full">
+                    <TiMicrophone/>
+                </div>
+            </div>
+            <div className="flex gap-5 items-center text-xl ">
+                <BsCameraVideo/>
+                <IoAppsSharp/>
+                <div className="relative">
+                   <BsBell/>
+                   <span className="absolute bottom-2 left-2 text-xs bg-red-600 rounded-full px-1">
+                     9+
+                   </span>
+                </div>
+                 <img
+                    src="https://yt3.ggpht.com/wgnEbc2Ec2JYkeyzUbiHzDlAFObI2Btwo2YRCEF1aCMBiRc5E_zWy8-URBQS3EMQ1yzzaGFR=s88-c-k-c0x00ffffff-no-rj-mo"
+                    className="w-9 h-9 rounded-full"
+                    alt="logo"
+                 />
             </div>
         </div>
     )
